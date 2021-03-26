521ffacdb484f98f74d5cd8596fbaa2c89b26a7d
dsanrails <77232860+dsanrails@users.noreply.github.com>
2021-03-26 21:25:12 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comments_params)
+    @prototype = @comment.prototype
+    if @comment.save
+      redirect_to prototype_path(@comment.prototype)
+    else
+      render "prototypes/show"
+    end
+  end
+  private
+  def comments_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+end
+
